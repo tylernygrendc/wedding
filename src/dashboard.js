65020eb427df61ba78cc905545740b013c7931a4
Tyler Nygren <tylernygren@Tylers-MBP.lan>
2022-11-05 12:24:43 -0400

    changes to dashboard

--- src/dashboard.js
+++ src/dashboard.js
@@ -19,29 +19,112 @@ signInAnonymously(auth);
 const db = getFirestore(app);
 
 window.addEventListener("load", () => {
-    let options = document.querySelectorAll("#functions button");
-    options[0].addEventListener("click", () => {
-        getAllGuests();
-    })
+    let functionSelector = document.querySelector("#functionSelector"),
+        start = document.querySelector("#start");
+    start.addEventListener("click", async () => {
+        switch(functionSelector.value){
+            case "getAllGuests":
+                getAllGuests();
+                break;
+            case "getAttending":
+                getAttending();
+            case "getNotAttending":
+                getAttending(false);
+                break;
+            case "getPurchased":
+                break;
+        }
+    });
 });
 
 // Show all guests and attending status
 
 async function getAllGuests() {
 
+    let table = document.querySelector("#output");
+    
+    while(table.firstChild) table.removeChild(table.firstChild);
+
+    const keys = ["name", "attending", "party"];
+
+    let tableHead = document.createElement("thead");
+
+    keys.forEach(key => {
+        let newCell = document.createElement("td"),
+            newText = document.createTextNode(key);
+        newCell.appendChild(newText);
+        tableHead.appendChild(newCell);
+    });
+
+    table.appendChild(tableHead);
+
     let querySnapshot = await getDocs(collection(db, "guests"));
+
+    querySnapshot.forEach(doc => {
+
+        let newRow = document.createElement("tr");
+        
+        keys.forEach(key => {
+            let newCell = document.createElement("td"),
+                newText = document.createTextNode(doc.data()[key]);
+            newCell.appendChild(newText);
+            newRow.appendChild(newCell);
+        });
+        table.appendChild(newRow);
+    });
+
+    sortTable(table);
+}
+
+async function getAttending(status=true) {
 
     let table = document.querySelector("#output");
     
     while(table.firstChild) table.removeChild(table.firstChild);
 
-    querySnapshot.forEach(doc => {
-        let newRow = document.createElement("tr");
-        for(var prop in doc.data()){
-            let newCell = document.createElement("td").appendChild(document.createTextNode(prop));
-            newRow.appendChild(newCell);
-        }
-        table.appendChild(newCell);
+    const keys = ["name", "attending", "party"];
+
+    let tableHead = document.createElement("thead");
+
+    keys.forEach(key => {
+        let newCell = document.createElement("td"),
+            newText = document.createTextNode(key);
+        newCell.appendChild(newText);
+        tableHead.appendChild(newCell);
     });
+
+    table.appendChild(tableHead);
+
+    let querySnapshot = await getDocs(query(collection(db, "guests"), where("attending","==", status)));
+
+    querySnapshot.forEach(doc => {
+
+        let newRow = document.createElement("tr");
+        
+        keys.forEach(key => {
+            let newCell = document.createElement("td"),
+                newText = document.createTextNode(doc.data()[key]);
+            newCell.appendChild(newText);
+            newRow.appendChild(newCell);
+        });
+        table.appendChild(newRow);
+    });
+
+    sortTable(table);
 }
-// Show all registry items and purchase status, with purchased by name
+
+function sortTable(table) {
+let rows, sorting = true, x, y;
+while (sorting) {
+        sorting = false;
+        rows = table.rows;
+        for (let i = 0; i < (rows.length - 1); i++) {
+            x = rows[i].getElementsByTagName("td")[0];
+            y = rows[i + 1].getElementsByTagName("td")[0];
+            if (String(x.innerHTML).split(" ")[1] > String(y.innerHTML).split(" ")[1]) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                sorting = true;
+            }
+        }
+    }
+}
